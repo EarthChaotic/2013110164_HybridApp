4c33719d5980f9ac9298e86a46d68bf91dfc7d77
b201student <b201student@B201COM6>
2022-12-21 08:33:01 +0700

    Add project files.

---
+++ TNI2013110164/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TNI2013110164
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //FlyoutPage
+            var FP = new FlyoutPage();
+            FP.Flyout = new MenuPage();
+            FP.Detail = new NavigationPage(new MainPage()) ;
+
+            MainPage = FP;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TNI2013110164/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TNI2013110164
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            sendButton.Clicked += SendButton_Clicked;
+
+            aSlider.ValueChanged += ASlider_ValueChanged;
+
+            userEntry.TextChanged += UserEntry_TextChanged;
+
+            aStepper.ValueChanged += AStepper_ValueChanged;
+
+            aSwitch.Toggled += ASwitch_Toggled;
+
+            colorPicker.SelectedIndexChanged += ColorPicker_SelectedIndexChanged;
+        }
+
+        private void ColorPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            yaLabel.Text = colorPicker.SelectedItem.ToString();
+        }
+
+        private void ASwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            
+            if(e.Value == true)
+            {
+                yaLabel.Text = "เปิด";
+            }
+            else if(e.Value == false){
+                yaLabel.Text = "ปิด";
+            }
+        }
+
+        private void AStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+           yaLabel.Text = e.NewValue.ToString();
+        }
+
+        private void UserEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            yaLabel.Text = userEntry.Text;
+        }
+
+        private void ASlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            yaLabel.Text = e.NewValue.ToString();
+        }
+
+        private void SendButton_Clicked(object sender, EventArgs e)
+        {
+            yaLabel.Text = userEntry.Text;
+        }
+    }
+}
